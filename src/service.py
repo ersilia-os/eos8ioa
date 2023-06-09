647fdf784767970d155c40dd244e8658fb274727
samuelmayna <samuelmayna@gmail.com>
2023-06-09 17:58:59 +0300

    Modified service.py to match model output and run.sh

--- src/service.py
+++ src/service.py
@@ -16,17 +16,40 @@ import csv
 CHECKPOINTS_BASEDIR = "checkpoints"
 FRAMEWORK_BASEDIR = "framework"
 
+
 def load_model(framework_dir, checkpoints_dir):
     mdl = Model()
     mdl.load(framework_dir, checkpoints_dir)
     return mdl
 
 
+def Float(x):
+    try:
+        return float(x)
+    except:
+        return None
+
+
+def String(x):
+    x = str(x)
+    if not x:
+        return None
+    if x == "nan":
+        return None
+    if x == "null":
+        return None
+    if x == "False":
+        return None
+    if x == "None":
+        return None
+    return x
+
+
 class Model(object):
     def __init__(self):
-        self.DATA_FILE = "data.csv"
-        self.PRED_FILE = "pred.csv"
-        self.RUN_FILE = "run.sh"
+        self.DATA_FILE = "_data.csv"
+        self.OUTPUT_FILE = "_output.csv"
+        self.RUN_FILE = "_run.sh"
         self.LOG_FILE = "run.log"
 
     def load(self, framework_dir, checkpoints_dir):
@@ -39,22 +62,20 @@ class Model(object):
     def set_framework_dir(self, dest):
         self.framework_dir = os.path.abspath(dest)
 
-    def calculate(self, smiles_list):
-        tmp_folder = tempfile.mkdtemp()
+    def run(self, input_list):
+        tmp_folder = tempfile.mkdtemp(prefix="eos-")
         data_file = os.path.join(tmp_folder, self.DATA_FILE)
-        pred_file = os.path.join(tmp_folder, self.PRED_FILE)
+        output_file = os.path.join(tmp_folder, self.OUTPUT_FILE)
         log_file = os.path.join(tmp_folder, self.LOG_FILE)
         with open(data_file, "w") as f:
-            f.write("smiles"+os.linesep)
-            for smiles in smiles_list:
-                f.write(smiles + os.linesep)
+            f.write("input" + os.linesep)
+            for inp in input_list:
+                f.write(inp + os.linesep)
         run_file = os.path.join(tmp_folder, self.RUN_FILE)
         with open(run_file, "w") as f:
             lines = [
-                "python {0}/calculate.py {1} {2}".format(
-                    self.framework_dir,
-                    data_file,
-                    pred_file
+                "bash {0}/run.sh {0} {1} {2}".format(
+                    self.framework_dir, data_file, output_file
                 )
             ]
             f.write(os.linesep.join(lines))
@@ -63,14 +84,19 @@ class Model(object):
             subprocess.Popen(
                 cmd, stdout=fp, stderr=fp, shell=True, env=os.environ
             ).wait()
-        with open(pred_file, "r") as f:
+        with open(output_file, "r") as f:
             reader = csv.reader(f)
             h = next(reader)
             R = []
             for r in reader:
-                R += [{"nplikeness": float(r[0]),
-                       "confidence": float(r[1])}]
-        return R
+                R += [
+                    {"outcome": [{"nplikeness": Float(r[0]),
+                       "confidence": Float(r[1])}]}
+                ]  # <-- EDIT: Modify according to type of output (Float, String...)
+        meta = {"outcome": h}
+        result = {"result": R, "meta": meta}
+        shutil.rmtree(tmp_folder)
+        return result
 
 
 class Artifact(BentoServiceArtifact):
@@ -123,8 +149,8 @@ class Artifact(BentoServiceArtifact):
 @artifacts([Artifact("model")])
 class Service(BentoService):
     @api(input=JsonInput(), batch=True)
-    def calculate(self, input: List[JsonSerializable]):
+    def run(self, input: List[JsonSerializable]):
         input = input[0]
-        smiles_list = [inp["input"] for inp in input]
-        output = self.artifacts.model.calculate(smiles_list)
+        input_list = [inp["input"] for inp in input]
+        output = self.artifacts.model.run(input_list)
         return [output]
